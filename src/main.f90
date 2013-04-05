51f00de5a244b3de8e2e53640607eca8a508b9a5
charlesswartz <charles.swartz@temple.edu>
2013-04-05 11:04:20 -0400

    Program Made More General and added integration sphere
    This program has been converted to make a general projection wavefunction
    onto spectrum of wavefunctions. The option to integrate this projection in a
    sphere of rcut around a defect postion was also added.

--- src/main.f90
+++ src/main.f90
@@ -1,3 +1,24 @@
+!
+! Author: Charles W. Swartz VI
+!         Temple University 
+!         March 2013
+!
+! Title: Wavefunction Projections
+!
+! Description: This Porgram will take a Single Wavefunction (The Projected
+! Wavefuntion) and compute its projections onto a number of the other
+! wavefunctons (Spectrum Wavefunctions).
+!
+!
+! Input: Input-File (From STDIN) with the following...
+!
+!
+! Important Note: the Files to be read in must be fortan binary (unformatted) files
+! containing the 3D complex wavefunction packaged into a single array (this will
+! be converted back into a 3D array following fortran column-dependent arrays).
+! These files MUST be named: <proj_root/spec_root><XXXX>.<proj_ext/spec_ext>
+!
+!
 !*****************************************************************************
 Program wanproj
 !*****************************************************************************
@@ -12,21 +33,25 @@ Program wanproj
    real(DP),parameter   :: ao  = 0.52917721_DP
    !
    complex(DP),allocatable :: read_1d(:),          &  !Initial 1D array for Wavefunction
-                              wan_wf(:,:,:),       &  !ix,iy,iz point for Wannier Wavefunction
-                              ks_wf(:,:,:)            !ix,iy,iz point for "Kohn-Sham" wavefunction
+                              spec_wf(:,:,:),      &  !ix,iy,iz point for Spectrum Wavefunctions
+                              proj_wf(:,:,:)          !ix,iy,iz point for Projected Wavefunction
 
    real(DP),allocatable ::    space(:,:,:,:),      &  !(ix, iy, iz, point value)  from FFT Grid
                               val(:),              &  !complete overlap
-                              val_wan(:),          &  !wannier Functions sum- check
-                              val_ks(:)               !Kohn Sham sum - check
+                              val_spec(:),         &  !Spectrum wavfunctions sum- check
+                              val_proj(:)             !Projected wavefunction sum - check
    !
    real(DP)                :: valt,                &  !"local" copy of overlap
-                              valt_wan,            &  !local copy of wannier sum
-                              valt_ks,             &  !local copy of Kohn-Sham sum
+                              valt_spec,           &  !local copy of spectrum sum
+                              valt_proj,           &  !local copy of projected sum
                               ans,                 &  !transfer value amoung processors
                               alat(3),             &  !lattice vetors
                               dr(3),               &  !interval distance between space points
-                              start_time,          &
+                              rcut,                &  !Cut-off radius in Angstroms (int_sphere)
+                              defect(3),           &  !Position of the defect Ion (int_sphere)
+                              disp(3),             &  !Individual component displacement from defect (int_sphere)
+                              dist2,               &  !Squared distance from defect (int_sphere)
+                              start_time,          &  
                               end_time
                            
    !
@@ -34,15 +59,13 @@ Program wanproj
                               ix, iy, iz,          &  !index for space/wavefunction
                               grid(3), grid_tot,   &  !Grid values and grid totals
                               nbsp,                &  !Number of States
-                              ks_state,            &  !Kohn-Sham state to calculate the overlap
-                              ks_offset,           &  !added value for the KS fort.* file
-                              wan_offset,          &  !added value for the wannier fort.* file
+                              proj_state,          &  !Projected Wavefunction state to calculate the overlap (see above)
                               ierr
    !
-   logical              :: print_xsf
+   logical              :: print_xsf, int_sphere
    !
-   character(len=100)   :: wan_root, ks_root, tempfile, x1, output, atomfile
-   character(len=10)    :: ks_ext, wan_ext
+   character(len=100)   :: spec_root, proj_root, tempfile, x1, output, atomfile
+   character(len=10)    :: spec_ext, proj_ext
    !
    !----------------------------------------------
    !For parallel execution
@@ -57,16 +80,17 @@ Program wanproj
    !----------------------------------------------
    !
    !Namelist for Standard Input
-   NAMELIST /wan_projections/ wan_root, ks_root, wan_offset, grid, nbsp, alat, ks_state, &
-                      ks_offset, ks_ext, wan_ext, output, print_xsf, atomfile
+   NAMELIST /projections/ spec_root, proj_root, grid, nbsp, alat, proj_state,      &
+                      spec_ext, proj_ext, output, print_xsf, atomfile,           &
+                      int_sphere, rcut, defect
    ! 
    !
    !
-   !Start Parallel environment Allocate/Initialize variables and read in KS Wavefunction
+   !Start Parallel environment Allocate/Initialize variables and read in Projected Wavefunction
    Call startup()
    !
    !vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-   !Loop overall the different wannier Function Files
+   !Loop overall the different Spectrum wavefunction Files
    !-------------------------------------------------------
    proc_loop: do np=1,per_proc,1
       !
@@ -74,8 +98,8 @@ Program wanproj
       if (me > nbsp) exit
       !
       !Determine and open the correct file
-      write(x1, '(I0)'), me + wan_offset 
-      tempfile = TRIM(wan_root)//TRIM(x1)//TRIM(wan_ext)
+      write(x1, '(I0)'), me 
+      tempfile = TRIM(spec_root)//TRIM(x1)//TRIM(spec_ext)
       open(unit=1, file=TRIM(tempfile),iostat=ierr, form='unformatted')
       if (ierr /= 0) then
          print *, 'Error: Opening ', tempfile
@@ -84,23 +108,23 @@ Program wanproj
       !
       !Read binary file and create the space and wavefunction files AND convert them to 3D
       read(1) read_1d
-      Call array_1D_to_3D (read_1d, wan_wf, grid, grid_tot)
+      Call array_1D_to_3D (read_1d, proj_wf, grid, grid_tot)
       close(1)
       !
       !If print_xsf if defined print out the xsf file
       if (print_xsf) then
          !
-         write(x1, '(I0)'), wan_offset+me
-         tempfile = TRIM(wan_root)//TRIM(x1) // '.xsf'
+         write(x1, '(I0)'), me
+         tempfile = TRIM(spec_root)//TRIM(x1) // '.xsf'
          !
-         call write_xsf(dble(wan_wf)**2, grid, tempfile, atomfile)
+         call write_xsf(dble(spec_wf)**2, grid, tempfile, atomfile)
          !
       endif
       !
       !Loop over all points add up sums
       valt = 0.0_DP
-      valt_wan = 0.0_DP
-      if(myid == 0) valt_ks  = 0.0_DP
+      valt_spec= 0.0_DP
+      if(myid == 0) valt_proj  = 0.0_DP
       !
       do iz=1, grid(3)
          do iy=1,grid(2)
@@ -109,14 +133,36 @@ Program wanproj
                !SPACE ARRAY
                space(ix,iy,iz,1:3) = (/(ix-1)*dr(1), (iy-1)*dr(2), (iz-1)*dr(3) /)
                !
-               !Calculate Overlap
-               valt = valt + abs(ks_wf(ix,iy,iz)) * abs(wan_wf(ix,iy,iz))
+               !Check to see if integration sphere is set-up
+               if (int_sphere) then
+                     !
+                     !Indivdual displacments
+                     do i=1,3,1
+                        disp(i) = space(ix,iy,iz,i) - defect(i) 
+                        disp(i) = disp(i) - NINT(disp(i)/alat(i))*alat(i)
+                     enddo
+                     !
+                     dist2 = disp(1)**2 + disp(2)**2 + disp(3)**2
+                     !
+                     !Sphere of integration Integration
+                     if ( dist2 < (rcut/ao)**2 ) then
+                        !
+                        !calculate overlap over this region
+                        valt = valt + abs(proj_wf(ix,iy,iz)) * abs(spec_wf(ix,iy,iz))
+                        !
+                     endif
+               else
+                  !
+                  !calculate overlap over the enitere space
+                  valt = valt + abs(proj_wf(ix,iy,iz)) * abs(spec_wf(ix,iy,iz))
+                  !
+               endif
+
+               !Check Spectrum wavefunction^2 norm
+               valt_spec = valt_spec + spec_wf(ix,iy,iz)*spec_wf(ix,iy,iz)
                !
-               !Check wan norm
-               valt_wan = valt_wan + wan_wf(ix,iy,iz)*wan_wf(ix,iy,iz)
-               !
-               !Check ks norm
-               if (myid == 0) valt_ks = valt_ks + ks_wf(ix,iy,iz)*ks_wf(ix,iy,iz)
+               !Check Porjected Wavefunction^2 norm
+               if (myid == 0) valt_proj = valt_proj + proj_wf(ix,iy,iz)*proj_wf(ix,iy,iz)
                !
             enddo
          enddo
@@ -124,13 +170,13 @@ Program wanproj
       !
       !Calculate Integrals
       valt = dble(valt/(grid(1)*grid(2)*grid(3)))**2
-      valt_wan = valt_wan/(grid(1)*grid(2)*grid(3))
-      if (myid ==0) valt_ks = valt_ks/(grid(1)*grid(2)*grid(3))
+      valt_spec = valt_spec/(grid(1)*grid(2)*grid(3))
+      if (myid ==0) valt_proj = valt_proj/(grid(1)*grid(2)*grid(3))
       !
       !Last loop Clean up
       if (np == per_proc) then
-         deallocate( wan_wf, read_1d, space )
-         if(allocated(ks_wf)) deallocate(ks_wf)
+         deallocate( spec_wf, read_1d, space )
+         if(allocated(proj_wf)) deallocate(proj_wf)
       endif
       !
       !--------------------------------------------
@@ -140,7 +186,7 @@ Program wanproj
       Call mpi_send_recv(me, myid, np, nproc, per_proc, remain, val, valt)
       !
       !
-      write(6,'(6X,"Wannier Function : ", I4, ", Integrated Charge : ",F14.5)') me, valt_wan 
+      write(6,'(6X,"Spectrum Wavefunction : ", I4, ", Integrated Charge : ",F14.5)') me, valt_spec
       !
    enddo proc_loop 
    !-------------------------------------------------------
@@ -171,7 +217,7 @@ Program wanproj
       end_time = MPI_Wtime()
       close(3)
       write(6,*)
-      write(6,'(5X,"Kohn-Sham Integrated Charge : ",F14.5)') valt_ks
+      write(6,'(5X,"Projected Wavefunction Integrated Charge : ",F14.5)') valt_proj
       write(6,*)
       Write(6,*) '    Total time: ', end_time - start_time
       write(6,*)
@@ -202,33 +248,33 @@ Program wanproj
          !
          !Defaults
          atomfile = ' '
+         int_sphere = .FALSE.
          print_xsf = .FALSE.
-         wan_offset = 0
-         ks_offset = 0
+         rcut = 1.0
          !
          !Clock
          if (myid == 0) then
             !
             start_time = MPI_Wtime()
             !
-            read(5,nml=wan_projections,IOSTAT=ierr)
+            read(5,nml=projections,IOSTAT=ierr)
             !
             call welcome()
             !
          endif
          !
-         Call MPI_BCAST(wan_root, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(wan_ext, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(ks_root, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(ks_ext, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(ks_state, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(wan_offset, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(ks_offset, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(spec_root, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(spec_ext, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(proj_root, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(proj_ext, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(proj_state, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
          Call MPI_BCAST(alat, 3, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
          Call MPI_BCAST(nbsp, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
          Call MPI_BCAST(grid, 3, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
-         Call MPI_BCAST(alat, 3, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
          Call MPI_BCAST(print_xsf, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(int_sphere, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(defect, 3, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+         Call MPI_BCAST(rcut, 1, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
          Call MPI_BCAST(atomfile, 100, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
          !
          if (ierr /= 0 ) then
@@ -237,6 +283,7 @@ Program wanproj
          !
          if (print_xsf .and. atomfile == ' ' ) then 
             if (myid == 0) write(*,*) ' ERORR: atomfile must be defined with print_xsf '
+            stop
          endif
          !
          !******Number jobs per processor (important for the mpi_send_recv)*****
@@ -246,21 +293,21 @@ Program wanproj
          !wavefunctions and space grids
          grid_tot = grid(1)*grid(2)*grid(3)
          allocate(read_1d(grid_tot))
-         allocate( wan_wf(grid(1), grid(2), grid(3))    )
-         allocate( ks_wf(grid(1), grid(2), grid(3))     )
+         allocate( spec_wf(grid(1), grid(2), grid(3))    )
+         allocate( proj_wf(grid(1), grid(2), grid(3))    )
          allocate( space(grid(1), grid(2), grid(3), 3) )
          allocate( val(nbsp)  )
-         if (myid == 0) allocate( val_ks(nbsp)   )
-         allocate( val_wan(nbsp)  )
+         if (myid == 0) allocate( val_proj(nbsp)   )
+         allocate( val_spec(nbsp)  )
          !
          !Initialize
          read_1d(:)     = 0.0_DP
-         wan_wf(:,:,:)  = 0.0_DP
-         ks_wf(:,:,:)   = 0.0_DP
+         spec_wf(:,:,:) = 0.0_DP
+         proj_wf(:,:,:) = 0.0_DP
          space(:,:,:,:) = 0.0_DP
          val(:)         = 0.0_DP
-         val_wan(:)     = 0.0_DP
-         if (myid == 0) val_ks(:)      = 0.0_DP
+         val_spec(:)    = 0.0_DP
+         if (myid == 0) val_proj(:)      = 0.0_DP
          !
          !Set up space with matching indexes 
          do i=1,3,1
@@ -272,15 +319,15 @@ Program wanproj
          if(myid == 0) then
             !
             !Determine and open the correct file
-            write(x1, '(I0)'), ks_offset+ks_state
-            tempfile = TRIM(ks_root)//TRIM(x1) // TRIM(ks_ext)
+            write(x1, '(I0)'), proj_state
+            tempfile = TRIM(proj_root)//TRIM(x1) // TRIM(proj_ext)
             open(unit=2, file=TRIM(tempfile),iostat=ierr, status='unknown', form='UNFORMATTED')
             !
             if (ierr /= 0) then
                print *, 'Error: Opening ', tempfile
                stop
             else 
-               write(*,'(/,5X,"Kohn-Sham state read from:",A50)') tempfile
+               write(*,'(/,5X,"Projected Wavefunctionread from:",A50)') tempfile
             endif
             !
             read(2) read_1d
@@ -290,18 +337,18 @@ Program wanproj
          endif
          !
          Call MPI_BCAST(read_1d, grid_tot, MPI_COMPLEX, 0, MPI_COMM_WORLD, ierr)
-         Call array_1D_to_3D (read_1d, ks_wf, grid, grid_tot)
+         Call array_1D_to_3D (read_1d, proj_wf, grid, grid_tot)
          !
          !If the print_xsf is set then print out the .xsf file for the KS State
          if(myid == 0) then
             if (print_xsf) then
                !
-               write(x1, '(I0)'), ks_offset+ks_state
-               tempfile = TRIM(ks_root)//TRIM(x1) // '.xsf'
+               write(x1, '(I0)'), proj_state
+               tempfile = TRIM(proj_root)//TRIM(x1) // '.xsf'
                !
-               call write_xsf(dble(ks_wf)**2, grid, tempfile, atomfile)
+               call write_xsf(dble(proj_wf)**2, grid, tempfile, atomfile)
                !
-               write(*,'(5X,"Kohn-Sham xsf :",A50)') tempfile
+               write(*,'(5X,"Projected Wavefunction xsf :",A50)') tempfile
                !
                close(3)
             endif
@@ -402,7 +449,7 @@ Program wanproj
       !-------------------------------------------------------
       !
       !-------------------------------------------------------
-      ! Write the KS and WAN Charge Densities (requries atomfile)
+      ! Write the Projected and Spsctrum Charge Densities (requries atomfile)
       !
       ! Format of atomfile (Note: bohr_or_Ang = 0 for bohr and 1 for Ang ):
       !  
@@ -504,21 +551,35 @@ Program wanproj
          !
          write(*,*) ' ' 
          write(*,'(2X,"-----------------------------------------------")')
-         write(*,'(2X,"             Wannier Projections               ")')
+         write(*,'(2X,"           Wavefunction Projections            ")')
          write(*,'(2X,"-----------------------------------------------")')
          write(*,*)
          write(*,'(3X,"Input Values:")')
-         write(*,'(3X,"Kohn Sham State  ", I4)') ks_state
-         write(*,'(3X,"Wannier States   ", I4)') nbsp
+         write(*,'(3X,"Projected State  ", I4)') proj_state
+         write(*,'(3X,"Spectrum States  ", I4)') nbsp
          write(*,'(3X,"Grid values      ", 1X, I4, I4, I4)') grid(1), grid(2), grid(3)
          write(*,'(3X,"Alat values      ", 3X, 3(F7.4,2X))') alat(1), alat(2), alat(3)
          write(*,*)
          write(*,'(3X,"Output File :      ",A20)') output
          write(*,*)
+
          if (print_xsf) Then
-            write(*,'(3X,"ALL Charge Densities will be converted to xsf Files")')
+            write(*,'(3X,"All Charge Densities will be converted to xsf Files")')
+            write(*,*)
+         else
+            write(*,'(3X,"No Charge Densities xsf Files will be created")')
             write(*,*)
          endif
+
+         if (int_sphere) Then
+            write(*,'(3X,"Projection over integration sphere, radius: ", F7.4, " Angstroms")') rcut
+            write(*,'(3X,"Defect Postion: ", 3(F7.4,2X))') rcut
+            write(*,*)
+         else
+            write(*,'(3X,"Projection over the entire space")')
+            write(*,*)
+         endif
+
          write(*,'(3X,"Total Number of Processors: ", I4)') nproc
          write(*,*)
          write(*,'(3X,"Program Start...")')
