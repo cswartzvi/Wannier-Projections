7f46b877a310184fccd7fd376b1ac9d741207a48
charlesswartz <charles.swartz@temple.edu>
2013-04-09 10:43:57 -0400

    Added maby tar file

---
+++ scripts/KS_tar.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl
+###########################################################
+#
+# Input
+# 1) KS Files root
+# 2) KS file ext
+# 3) peaks file
+#
+###########################################################
+
+use strict;
+use warnings;
+
+#root and ext of the KS Files
+my $root = shift @ARGV;
+my $ext = shift @ARGV;
+
+my $tar_line = "tar -cvzf projections.tgz ";
+for (<>){
+
+   my $num = (split)[1];
+
+   $tar_line .= " ".$root.$num.$ext." ";
+
+}
+
+system ("$tar_line");
